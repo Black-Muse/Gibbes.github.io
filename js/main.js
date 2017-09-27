ce3607fa71832c451e24d5e9300144b8f2cf9cf5
Michael Gibbes <mgibbes@berkeley.edu>
2017-09-27 04:48:58 -0700

    Enforce controls

--- js/main.js
+++ js/main.js
@@ -29,8 +29,12 @@ ctx.font = 'bold 12pt Arial';
 ctx.fillStyle = '#933';
 var maxWidth = 200;
 var text = "Welcome to Alien Nations! Each player controls a deck of 5 unique cards. Press 'Begin' to initialize the deck.";
-wrapText(text, ctx, 20, 30, maxWidth);
-
+function insert(text) {
+	"use strict";
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	wrapText(text, ctx, 20, 30, maxWidth);
+}
+insert(text);
 
 /** INIT PLAYER MATERIALS **/
 var img1 = document.getElementById("img1");
@@ -40,15 +44,29 @@ var inv1 = all_races.splice();
 var inv2 = all_races.splice();
 var card1 = all_races.length - 1;
 var card2 = all_races.length - 1;
+var reroll1 = false;
+var reroll2 = false;
 function init_inv() {
 	"use strict";
 	
 	return all_races.slice();
 }
+function deactivate(button) {
+	"use strict";
+	button.style.background = "#eee";
+	button.onmouseover = function () {button.style.backgroundColor = "#eee";};
+	button.onmouseout = function () {button.style.backgroundColor = "#eee";};
+}
+function activate(button) {
+	"use strict";
+	button.style.background = "salmon";
+	button.onmouseover = function () {button.style.backgroundColor = "lightsalmon";};
+	button.onmouseout = function () {button.style.backgroundColor = "salmon";};
+}
 var button1 = document.getElementById("button1");
 var button2 = document.getElementById("button2");
-var reroll1 = false;
-var reroll2 = false;
+deactivate(button1);
+deactivate(button2);
 
 
 /** BEGIN/RESET FUNCTIONALITY **/
@@ -58,11 +76,15 @@ control.addEventListener("click", function () {
 	"use strict";
 	
 	if (!inGame) {
-		control.innerHTML = "Restart";
+		deactivate(control);
+		control.innerHTML = "Play";
 		inGame = true;
+		activate(button1);
+		activate(button2);
+		inv1 = init_inv();
+		inv2 = init_inv();
+		insert("May the best faction win! Either side needs to roll at least once before each battle commences.");
 	}
-	inv1 = init_inv();
-	inv2 = init_inv();
 });
 
 /** ROLL FUNCTIONALITY **/
@@ -97,10 +119,12 @@ button1.addEventListener("click", function () {
 	if (inGame && reroll1 !== -1) {
 		roll(1, Math.floor((Math.random() * 5) + 10));
 		if (!reroll1) {
+			if (reroll2) {activate(control);}
 			reroll1 = true;
 			button1.innerHTML = "Reroll";
 		} else {
-			reroll1 = true;
+			reroll1 = -1;
+			deactivate(button1);
 		}
 	}
 });
@@ -109,10 +133,12 @@ button2.addEventListener("click", function () {
 	if (inGame && reroll2 !== -1) {
 		roll(2, Math.floor((Math.random() * 5) + 10));
 		if (!reroll2) {
+			if (reroll1) {activate(control);}
 			reroll2 = true;
 			button2.innerHTML = "Reroll";
 		} else {
-			reroll2 = true;
+			reroll2 = -1;
+			deactivate(button2);
 		}
 	}
 });
