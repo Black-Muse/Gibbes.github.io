1388ddf1edfa6df9a7408fb5d5ff863a117e43ee
Michael Gibbes <mgibbes@berkeley.edu>
2017-10-03 22:54:46 -0700

    BEAUTIFY

---
+++ js/game.js
@@ -0,0 +1,11 @@
+/** INIT START SCREEN **/
+var aliens = [];
+for (var i = 1; i <= 10; i++) {
+	aliens.push(document.getElementById("b" + i));
+}
+
+/** ANIMATION FUNCTIONS **/
+/** FUCK**/
+
+/** ADD LISTENERS **/
+/** DOUBLE FUCK **/
