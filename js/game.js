e08cc2985c1b3a303eabbc2d81d716ffcf72b2b5
Michael Gibbes <mgibbes@berkeley.edu>
2017-10-04 02:00:15 -0700

    fuck life

--- js/game.js
+++ js/game.js
@@ -1,11 +1,48 @@
+/*jshint strict:false */
+
 /** INIT START SCREEN **/
 var aliens = [];
 for (var i = 1; i <= 10; i++) {
 	aliens.push(document.getElementById("b" + i));
 }
+var extra_card = "";
 
-/** ANIMATION FUNCTIONS **/
-/** FUCK**/
+
+/** BEGIN FUNCTION **/
+function begin(elem) {
+	"use strict";
+	
+	extra_card = elem.className;
+	document.getElementById("header").innerHTML = "<h1>Alien Nations</h1>";
+	elem.parentNode.parentNode.removeChild(elem.parentNode);
+	
+	var test = document.createElement("test");
+
+	test.innerHTML = 
+		'<div class="grid">'+
+			'<div id = "b1" class="Asherog">Asherog - Destroyer<img src="img/Asherog.jpg"></div>'+
+			'<div id = "b2" class="Arka">Arka - Scholar<img src="img/Arka.jpg"></div>'+
+			'<div id = "b3" class="Astren">Astren - Creator<img src="img/Astren.jpg"></div>'+
+			'<div id = "b4" class="Benusi">Benusi - Trucker<img src="img/Benusi.jpg"></div>'+
+			'<div id = "b5" class="Buzari">Buzari - Merchant<img src="img/Buzari.jpg"></div>'+
+			'<div id = "b6" class="Charn">Charn - Telepath<img src="img/Charn.jpg"></div>'+
+			'<div id = "b7" class="Leonid">Leonid - Slaver<img src="img/Leonid.jpg"></div>'+
+			'<div id = "b8" class="Rachnian">Rachnian - Pirate<img src="img/Rachnian.jpg"></div>'+
+			'<div id = "b9" class="Suronian">Suronian - Warrior<img src="img/Suronian.jpg"></div>'+
+			'<div id = "b10" class="Thuvix">Thuvix - Mechanic<img src="img/Thuvix.jpg"></div>'+
+		'</div>';
+	
+	document.getElementById("body").insertBefore(test, document.getElementById("footer"));
+}
 
 /** ADD LISTENERS **/
-/** DOUBLE FUCK **/
+aliens[0].addEventListener("click", function() {begin(aliens[0]);});
+aliens[1].addEventListener("click", function() {begin(aliens[1]);});
+aliens[2].addEventListener("click", function() {begin(aliens[2]);});
+aliens[3].addEventListener("click", function() {begin(aliens[3]);});
+aliens[4].addEventListener("click", function() {begin(aliens[4]);});
+aliens[5].addEventListener("click", function() {begin(aliens[5]);});
+aliens[6].addEventListener("click", function() {begin(aliens[6]);});
+aliens[7].addEventListener("click", function() {begin(aliens[7]);});
+aliens[8].addEventListener("click", function() {begin(aliens[8]);});
+aliens[9].addEventListener("click", function() {begin(aliens[9]);});
