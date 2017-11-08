fe0f4c1f2260d27b0b97c0e129566f30c04a1818
Michael Gibbes <mgibbes@berkeley.edu>
2017-11-08 00:52:28 -0800

    AHAFHSDHCAHSDHF

---
+++ Alien Nations/Assets/scripts/HUD.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HUD : MonoBehaviour {
+
+    float transparency;
+    bool up;
+
+	// Use this for initialization
+	void Start () {
+        transparency = 1;
+        up = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transparency >= 1)
+        {
+            up = false;
+        }
+        else if (transparency <= 0)
+        {
+            up = true;
+        }
+        if (up)
+        {
+            transparency += 0.01f;
+        }
+        else
+        {
+            transparency -= 0.01f;
+        }
+        Color c = GetComponent<SpriteRenderer>().color;
+        c.a = transparency;
+        GetComponent<SpriteRenderer>().color = c;
+	}
+}
