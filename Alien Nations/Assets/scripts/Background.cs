21ad5e71692b1e99524eaf1971e3620ca5931d1f
Michael Gibbes <mgibbes@berkeley.edu>
2017-11-08 08:52:29 -0800

    AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH

---
+++ Alien Nations/Assets/scripts/Background.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Background : MonoBehaviour {
+
+    public static float distance;
+
+	// Use this for initialization
+	void Start () {
+        distance = 0.003f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position = new Vector2(transform.position.x + distance / 4, transform.position.y);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.position = new Vector2(transform.position.x - distance, transform.position.y);
+        }
+    }
+}
