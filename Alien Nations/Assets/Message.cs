87fdb9b80ba63dfdccbf3d85121061d4bc1f144b
Michael Gibbes <mgibbes@berkeley.edu>
2017-11-14 16:42:59 -0800

    Implement text component

---
+++ Alien Nations/Assets/Message.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Message : MonoBehaviour {
+
+    float transparency;
+    bool up;
+
+    // Use this for initialization
+    void Start () {
+        transparency = 1;
+        up = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (transparency >= 1)
+        {
+            up = false;
+        }
+        else if (transparency <= 0)
+        {
+            up = true;
+        }
+        if (up)
+        {
+            transparency += 0.01f;
+        }
+        else
+        {
+            transparency -= 0.01f;
+        }
+        Color c = GetComponent<Text>().color;
+        c.a = transparency;
+        GetComponent<Text>().color = c;
+
+        if (Mathf.Abs(transform.position.x) > 20)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Alien Nations/Assets/scripts/Planet.cs
+++ Alien Nations/Assets/scripts/Planet.cs
@@ -1,16 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Planet : MonoBehaviour {
+
+    public Text messageSpace;
+    Text message;
+    public string planet_name;
+    GameObject canvas;
 
 	// Use this for initialization
 	void Start () {
         transform.localScale = new Vector3(0.25f, 0.25f, 0.25f);
-	}
+        message = Instantiate(messageSpace);
+        canvas = GameObject.FindGameObjectWithTag("CanvasUI");
+        message.transform.SetParent(canvas.transform, false);
+        message.color = Color.white;
+    }
 	
 	// Update is called once per frame
 	void Update () {
+        if (transform.position.magnitude < 5)
+        {
+            message.text = "[E] Enter " + planet_name;
+        }
+        else
+        {
+            message.text = "";
+        }
 		if (Mathf.Abs(transform.position.x) > 20)
         {
             Destroy(gameObject);
