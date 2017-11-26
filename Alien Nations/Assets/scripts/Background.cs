94fd51d7669a4f866d73e4982cdb49715d5483d4
Michael Gibbes <mgibbes@berkeley.edu>
2017-11-25 17:35:12 -0800

    Fix background

--- Alien Nations/Assets/scripts/Background.cs
+++ Alien Nations/Assets/scripts/Background.cs
@@ -4,25 +4,9 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class Background : MonoBehaviour {
-
-    public static float distance;
-    public Transform background_anchor;
-
-    // Use this for initialization
-    void Start () {
-        distance = 0.01f;
-	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.position = new Vector2(transform.position.x - distance, transform.position.y);
-        }
-        else if (Input.GetKey(KeyCode.A))
-        {
-            transform.position = new Vector2(transform.position.x + distance / 4, transform.position.y);
-        }
-        
+        transform.position = new Vector2(transform.position.x - Spurdo.velocity.x / 8, transform.position.y);
     }
 }
