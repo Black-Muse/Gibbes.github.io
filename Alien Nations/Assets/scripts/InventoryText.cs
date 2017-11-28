91f78080f9729f069b6af4986a4d533e940d785a
Michael Gibbes <mgibbes@berkeley.edu>
2017-11-28 02:20:53 -0800

    Implement inventory

---
+++ Alien Nations/Assets/scripts/InventoryText.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryText : MonoBehaviour {
+
+    int value;
+    Text txt;
+    public float transparency;
+
+    // Use this for initialization
+    void Start () {
+        retrieveValue();
+        txt = GetComponent<Text>();
+        transparency = 0;
+        Color c = txt.color;
+        c.a = transparency;
+        txt.color = c;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (gameObject.tag != "Untagged")
+        {
+            txt.text = "  " + gameObject.tag + ": " + value;
+            retrieveValue();
+        }
+        Color c = txt.color;
+        c.a = transparency;
+        txt.color = c;
+        if (Pause.inv)
+        {
+            fadeIn();
+        }
+        else
+        {
+            fadeOut();
+        }
+    }
+
+    void retrieveValue ()
+    {
+        switch (gameObject.tag)
+        {
+            case "Fur":
+                value = metadata.fur;
+                break;
+            case "Wood":
+                value = metadata.wood;
+                break;
+            case "Spice":
+                value = metadata.spice;
+                break;
+            case "Helium":
+                value = metadata.helium;
+                break;
+            case "Hydrogen":
+                value = metadata.hydrogen;
+                break;
+            case "Iron":
+                value = metadata.iron;
+                break;
+        }
+    }
+
+    void fadeIn()
+    {
+        if (transparency < 1)
+        {
+            transparency += 0.05f;
+        }
+    }
+
+    void fadeOut()
+    {
+        if (transparency > 0)
+        {
+            transparency -= 0.05f;
+        }
+    }
+}
--- Alien Nations/Assets/scripts/metadata.cs
+++ Alien Nations/Assets/scripts/metadata.cs
@@ -8,4 +8,10 @@ public class metadata : MonoBehaviour {
     public static float man = 220;
     public static float bel = 100;
     public static float dur = 0;
+    public static int fur = 0;
+    public static int wood = 0;
+    public static int spice = 0;
+    public static int helium = 0;
+    public static int hydrogen = 0;
+    public static int iron = 0;
 }
